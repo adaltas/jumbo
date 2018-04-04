40a394e51bf3ba1f4a72a3bc5544ad761e46be76
Xavier Hermand <xavier@adaltas.com>
2018-04-04 17:51:19 +0200

    Add create_cluster command and amazing blink

---
+++ jumbo/utils/clusters.py
@@ -0,0 +1,26 @@
+import click
+
+import os
+import pathlib
+from distutils.dir_util import copy_tree
+
+JUMBODIR=os.path.expanduser('~/.jumbo/')
+
+
+def check_cluster(name):
+    return os.path.isdir(JUMBODIR + name)
+
+
+def create_cluster(name):
+    if check_cluster(name):
+        click.echo(click.style('Cluster already exists!', fg='red'), err=True)
+        return False
+
+    pathlib.Path(JUMBODIR + name).mkdir(parents=True)
+    copy_tree('data/empty', JUMBODIR+name)
+
+    return True
+
+
+
+
