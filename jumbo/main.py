e243940ee102e9efb74e11d39d3d2a1be3527e53
Xavier Hermand <xavier@adaltas.com>
2018-04-05 15:06:33 +0200

    Fix line lentgh to 79 to follow PEP8

--- jumbo/main.py
+++ jumbo/main.py
@@ -9,13 +9,15 @@ from jumbo.utils import clusters, session
 @click.pass_context
 def jumbo(ctx, cluster):
     """
-    Execute a Jumbo command. If no command is passed, start the Jumbo shell interactive mode.
+    Execute a Jumbo command.
+    If no command is passed, start the Jumbo shell interactive mode.
     """
 
     # Create the shell
     sh = Shell(prompt=click.style('jumbo > ', fg='green'),
-               intro=click.style('Welcome to the jumbo shell v0.1.3.3.7!', blink=True, fg='cyan'))
-    # Save the shell in the click context (allows to modify its prompt later on)
+               intro=click.style('Welcome to the jumbo shell v0.1.3.3.7!',
+                                blink=True, fg='cyan'))
+    # Save the shell in the click context (to modify its prompt later on)
     ctx.meta['jumbo_shell'] = sh.shell
     # Register commands that can be used in the shell
     sh.add_command(create)
@@ -24,11 +26,12 @@ def jumbo(ctx, cluster):
     # If cluster exists, save it to svars (session variable) and adapt prompt
     if cluster:
         if not clusters.check_cluster(cluster):
-            click.echo(
-                'This cluster does not exist. Use `create NAME` to create it.', err=True)
+            click.echo('This cluster does not exist.'
+                       ' Use `create NAME` to create it.', err=True)
         else:
             session.svars['cluster'] = cluster
-            ctx.meta['jumbo_shell'].prompt = click.style('jumbo (%s) > ' % cluster, fg='green')
+            ctx.meta['jumbo_shell'].prompt = click.style(
+                'jumbo (%s) > ' % cluster, fg='green')
 
     # Run the command, or the shell if no command is passed
     sh.invoke(ctx)
